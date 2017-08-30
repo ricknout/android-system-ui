10de290d0390d54caac6dfbb357875223a90a1de
Nick Rout <nicholasrout@gmail.com>
2017-08-30 17:25:36 +0200

    Add static shortcut and art

---
+++ app/src/main/java/com/nickrout/systemui/ShortcutActivity.java
@@ -0,0 +1,13 @@
+package com.nickrout.systemui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShortcutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shortcut);
+    }
+}
